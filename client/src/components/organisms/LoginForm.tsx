1daeb86d27e8d286638210f917dc20635178211c
thexeromin <thexeromin@gmail.com>
2025-02-14 13:47:58 +0530

    remove: forgot password

--- client/src/components/organisms/LoginForm.tsx
+++ client/src/components/organisms/LoginForm.tsx
@@ -65,15 +65,7 @@ export default function LoginForm() {
             name="password"
             render={({ field }) => (
               <FormItem>
-                <div className="flex items-center">
-                  <FormLabel>Password</FormLabel>
-                  <Link
-                    to="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </Link>
-                </div>
+                <FormLabel>Password</FormLabel>
                 <FormControl>
                   <Input type="password" placeholder="*****" {...field} />
                 </FormControl>
